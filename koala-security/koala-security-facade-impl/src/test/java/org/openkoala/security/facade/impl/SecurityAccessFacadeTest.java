62759f7dda40386403ee726a64bcc3a561e4961c
liujun <liujun2008116@gmail.com>
2014-09-13 16:36:01 +0800

    add: 提交第一部分代码

---
+++ koala-security/koala-security-facade-impl/src/test/java/org/openkoala/security/facade/impl/SecurityAccessFacadeTest.java
@@ -0,0 +1,369 @@
+package org.openkoala.security.facade.impl;
+
+import static org.junit.Assert.*;
+import static org.openkoala.security.facade.impl.util.EntitiesHelper.*;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.inject.Inject;
+
+import org.dayatang.querychannel.Page;
+import org.junit.Before;
+import org.junit.Test;
+import org.openkoala.koala.commons.InvokeResult;
+import org.openkoala.security.core.domain.MenuResource;
+import org.openkoala.security.core.domain.PageElementResource;
+import org.openkoala.security.core.domain.Permission;
+import org.openkoala.security.core.domain.Role;
+import org.openkoala.security.core.domain.UrlAccessResource;
+import org.openkoala.security.core.domain.User;
+import org.openkoala.security.facade.SecurityAccessFacade;
+import org.openkoala.security.facade.dto.*;
+import org.openkoala.security.facade.impl.assembler.*;
+
+
+/**
+ * 完善测试 对其测试结果进行断言。
+ */
+public class SecurityAccessFacadeTest extends AbstractFacadeIntegrationTestCase{
+
+	@Inject
+	private SecurityAccessFacade securityAccessFacade;
+	
+	private User user;
+	private UserDTO userDTO;
+	
+	private Role role;
+	private RoleDTO roleDTO;
+	
+	private Permission permission;
+	private PermissionDTO permissionDTO;
+	
+	private MenuResource menuResource;
+	
+	private UrlAccessResource urlAccessResource;
+	private UrlAccessResourceDTO urlAccessResourceDTO;
+	
+	private PageElementResource pageElementResource;
+	private PageElementResourceDTO pageElementResourceDTO;
+	
+	private int currentPage = 0;
+	private int pageSize = 10;
+	
+	@Before
+	public void setUp(){
+		user = initUser();
+		user.save();
+		userDTO = UserAssembler.toUserDTO(user);
+		
+		role = initRole();
+		role.save();
+		roleDTO = RoleAssembler.toRoleDTO(role);
+		
+		user.grant(role);
+		
+		permission = initPermission();
+		permission.save();
+		permissionDTO = PermissionAssembler.toPermissionDTO(permission);
+		
+		menuResource = initMenuResource();
+		menuResource.save();
+		
+		role.addSecurityResource(menuResource);
+		permission.addSecurityResource(menuResource);
+		
+		urlAccessResource = initUrlAccessResource();
+		urlAccessResource.save();
+		urlAccessResourceDTO = UrlAccessResourceAssembler.toUrlAccessResourceDTO(urlAccessResource);
+		
+		pageElementResource = initPageElementResource();
+		pageElementResource.save();
+		pageElementResourceDTO = PageElementResourceAssembler.toPageElementResourceDTO(pageElementResource);
+		
+		role.addSecurityResource(urlAccessResource);
+		permission.addSecurityResource(urlAccessResource);
+		
+		role.addSecurityResource(pageElementResource);
+		permission.addSecurityResource(pageElementResource);
+		
+		role.addPermission(permission);
+	}
+	
+	@Test
+	public void testGetUserDtoById() {
+		Long userId = user.getId();
+		assertNotNull(userId);
+		UserDTO getUserDTO = securityAccessFacade.getUserById(userId);
+		assertNotNull(getUserDTO);
+		assertUserDTO(userDTO, getUserDTO);
+	}
+
+	@Test
+	public void testGetUserDtoByUserAccount() {
+		UserDTO getUserDTO = securityAccessFacade.getUserByUserAccount(user.getUserAccount());
+		assertNotNull(getUserDTO);
+		assertUserDTO(userDTO, getUserDTO);
+	}
+
+	@Test
+	public void testFindRoleDtosByUserAccount() {
+		List<RoleDTO> roleDTOs = (List<RoleDTO>) securityAccessFacade.findRolesByUserAccount(user.getUserAccount()).getData();
+		assertNotNull(roleDTOs);
+		assertTrue(roleDTOs.size() == 1);
+	}
+
+	@Test
+	public void testFindPermissionsByUserAccountAndRoleName() {
+		Set<PermissionDTO> results =  securityAccessFacade.findPermissionsByUserAccountAndRoleName(user.getUserAccount(), role.getName());
+		assertFalse(results.isEmpty());
+		assertTrue(results.size() == 1);
+	}
+
+	@Test
+	public void testFindMenuResourceDtoByUserAccount() {
+		List<MenuResourceDTO> results = securityAccessFacade.findMenuResourceByUserAccount(user.getUserAccount());
+		assertFalse(results.isEmpty());
+		assertTrue(results.size() == 1);
+	}
+
+	@Test
+	public void testFindMenuResourceDTOByUserAccountAsRole() {
+		List<MenuResourceDTO> results = (List<MenuResourceDTO>) securityAccessFacade.findMenuResourceByUserAsRole(user.getUserAccount(), role.getName()).getData();
+		assertFalse(results.isEmpty());
+		System.out.println(results);
+		assertTrue(results.size() == 1);
+	}
+	
+	@Test
+	public void testFindAllMenusTree() {
+		List<MenuResourceDTO> results = (List<MenuResourceDTO>) securityAccessFacade.findAllMenusTree().getData();
+		assertFalse(results.isEmpty());
+		assertTrue(results.size() == 1);
+	}
+
+	@Test
+	public void testFindMenuResourceTreeSelectItemByRoleId() {
+		List<MenuResourceDTO> results  = (List<MenuResourceDTO>) securityAccessFacade.findMenuResourceTreeSelectItemByRoleId(role.getId()).getData();
+		assertFalse(results.isEmpty());
+		assertTrue(results.size() == 1);
+	}
+
+
+	@Test
+	public void testFindPermissionDTOsByMenuOrUrl() {
+		Set<PermissionDTO> results = securityAccessFacade.findPermissionsByMenuOrUrl();
+		assertNotNull(results);
+		assertTrue(results.size() == 2);
+	}
+
+	@Test
+	public void testFindRoleDTOsByMenuOrUrl() {
+		Set<RoleDTO> results = securityAccessFacade.findRolesByMenuOrUrl();
+		assertNotNull(results);
+		assertTrue(results.size() == 2);
+	}
+
+	@Test
+	public void testFindAllUrlAccessResources() {
+		List<UrlAuthorityDTO> result = securityAccessFacade.findAllUrlAccessResources();
+		assertNotNull(result);
+		assertTrue(result.size() == 1);
+        UrlAuthorityDTO urlAuthority = result.get(0);
+		Set<String> roles = urlAuthority.getRoles();
+		Set<String> permissions = urlAuthority.getPermissions();
+		assertNotNull(roles);
+		assertNotNull(permissions);
+        assertFalse(roles.isEmpty());
+        assertFalse(permissions.isEmpty());
+        assertTrue(roles.size() == 1);
+        assertTrue(permissions.size() == 1);
+        assertTrue(roles.contains(role.getName()));
+        assertTrue(permissions.contains(permission.getIdentifier()));
+	}
+
+	@Test
+	public void testPagingQueryUsers() {
+		Page<UserDTO> userDTOPages = (Page<UserDTO>) securityAccessFacade.pagingQueryUsers(currentPage, pageSize, userDTO).getData();
+		assertFalse(userDTOPages.getData().isEmpty());
+		assertTrue(userDTOPages.getResultCount() == 1);
+	}
+
+	@Test
+	public void testPagingQueryRoles() {
+		Page<RoleDTO> roleDTOPages = (Page<RoleDTO>) securityAccessFacade.pagingQueryRoles(currentPage, pageSize, roleDTO).getData();
+		assertFalse(roleDTOPages.getData().isEmpty());
+		assertTrue(roleDTOPages.getResultCount() == 1);
+	}
+
+	@Test
+	public void testPagingQueryPermissions() {
+		Page<PermissionDTO> permissionDTOPages = (Page<PermissionDTO>) securityAccessFacade.pagingQueryPermissions(currentPage, pageSize, permissionDTO).getData();
+		assertFalse(permissionDTOPages.getData().isEmpty());
+		assertTrue(permissionDTOPages.getResultCount() == 1);
+	}
+
+	@Test
+	public void testPagingQueryNotGrantRolesConditionIsNull() {
+		Role role = new Role("PagingQueryNotGrantRolesIntIntPermissionDTOLong");
+		role.save();
+		Page<RoleDTO> roleDTOPages = (Page<RoleDTO>) securityAccessFacade.pagingQueryNotGrantRoles(currentPage, pageSize, new RoleDTO(null, ""), user.getId()).getData();
+		assertFalse(roleDTOPages.getData().isEmpty());
+		assertTrue(roleDTOPages.getPageCount() == 1);
+	}
+	
+	@Test
+	public void testPagingQueryNotGrantRoles() {
+		Role role = new Role("PagingQueryNotGrantRolesIntIntPermissionDTOLong");
+		role.save();
+		Page<RoleDTO> roleDTOPages = (Page<RoleDTO>) securityAccessFacade.pagingQueryNotGrantRoles(currentPage, pageSize, RoleAssembler.toRoleDTO(role), user.getId()).getData();
+		assertFalse(roleDTOPages.getData().isEmpty());
+		assertTrue(roleDTOPages.getPageCount() == 1);
+	}
+
+	@Test
+	public void testPagingQueryGrantPermissionByUserId() {
+		Page<PermissionDTO> results = (Page<PermissionDTO>) securityAccessFacade.pagingQueryGrantPermissionByUserId(currentPage, pageSize, user.getId()).getData();
+		assertTrue(results.getData().isEmpty());
+	}
+
+	@Test
+	public void testPagingQueryGrantRolesByUserId() {
+		Page<RoleDTO> results = (Page<RoleDTO>) securityAccessFacade.pagingQueryGrantRolesByUserId(currentPage, pageSize, user.getId()).getData();
+		assertFalse(results.getData().isEmpty());
+		assertTrue(results.getPageCount() == 1);
+	}
+
+	@Test
+	public void testPagingQueryNotGrantPermissionsByRoleId() {
+		Permission permission = new Permission("testPagingQueryNotGrantPermissionsByRoleId", "testPagingQueryNotGrantPermissionsByRoleId");
+		permission.save();
+		Page<PermissionDTO> results = (Page<PermissionDTO>) securityAccessFacade.pagingQueryNotGrantPermissionsByRoleId(currentPage, pageSize, role.getId()).getData();
+		assertFalse(results.getData().isEmpty());
+		assertTrue(results.getPageCount() == 1);
+	}
+
+	@Test
+	public void testPagingQueryGrantPermissionsByRoleId() {
+		Page<PermissionDTO> results = (Page<PermissionDTO>) securityAccessFacade.pagingQueryGrantPermissionsByRoleId(currentPage, pageSize, role.getId()).getData();
+		assertFalse(results.getData().isEmpty());
+		assertTrue(results.getPageCount() == 1);
+	}
+
+	@Test
+	public void testPagingQueryUrlAccessResources() {
+		Page<UrlAccessResourceDTO> results = (Page<UrlAccessResourceDTO>) securityAccessFacade.pagingQueryUrlAccessResources(currentPage, pageSize, urlAccessResourceDTO).getData();
+		assertFalse(results.getData().isEmpty());
+		assertTrue(results.getPageCount() == 1);
+	}
+
+	@Test
+	public void testPagingQueryGrantUrlAccessResourcesByRoleId() {
+		Page<UrlAccessResourceDTO> results = (Page<UrlAccessResourceDTO>) securityAccessFacade.pagingQueryGrantUrlAccessResourcesByRoleId(currentPage, pageSize, role.getId()).getData();
+		assertFalse(results.getData().isEmpty());
+		assertTrue(results.getPageCount() == 1);
+	}
+
+	@Test
+	public void testPagingQueryNotGrantUrlAccessResourcesByRoleId() {
+		UrlAccessResource urlAccessResource = new UrlAccessResource("testPagingQueryNotGrantUrlAccessResourcesByRoleId", "testPagingQueryNotGrantUrlAccessResourcesByRoleId");
+		urlAccessResource.save();
+		Page<UrlAccessResourceDTO> results = (Page<UrlAccessResourceDTO>) securityAccessFacade.pagingQueryNotGrantUrlAccessResourcesByRoleId(currentPage, pageSize, role.getId()).getData();
+		assertFalse(results.getData().isEmpty());
+		assertTrue(results.getPageCount() == 1);
+	}
+
+	@Test
+	public void testPagingQueryGrantPermissionsByUrlAccessResourceId() {
+		Page<PermissionDTO> results = (Page<PermissionDTO>) securityAccessFacade.pagingQueryGrantPermissionsByUrlAccessResourceId(currentPage, pageSize, urlAccessResource.getId()).getData();
+		assertFalse(results.getData().isEmpty());
+		assertTrue(results.getPageCount() == 1);
+	}
+
+	@Test
+	public void testPagingQueryNotGrantPermissionsByUrlAccessResourceId() {
+		Permission permission = new Permission("testPagingQueryNotGrantPermissionsByUrlAccessResourceId", "testPagingQueryNotGrantPermissionsByUrlAccessResourceId");
+		permission.save();
+		Page<PermissionDTO> results = (Page<PermissionDTO>) securityAccessFacade.pagingQueryNotGrantPermissionsByUrlAccessResourceId(currentPage, pageSize, urlAccessResource.getId()).getData();
+		assertFalse(results.getData().isEmpty());
+		assertTrue(results.getPageCount() == 1);
+	}
+
+	@Test
+	public void testPagingQueryGrantPermissionsByMenuResourceId() {
+		Page<PermissionDTO> results = (Page<PermissionDTO>) securityAccessFacade.pagingQueryGrantPermissionsByMenuResourceId(currentPage, pageSize, menuResource.getId()).getData();
+		assertFalse(results.getData().isEmpty());
+		assertTrue(results.getPageCount() == 1);
+	}
+
+	@Test
+	public void testPagingQueryNotGrantPermissionsByMenuResourceId() {
+		Permission permission = new Permission("testPagingQueryNotGrantPermissionsByMenuResourceId", "testPagingQueryNotGrantPermissionsByMenuResourceId");
+		permission.save();
+		Page<PermissionDTO> results = (Page<PermissionDTO>) securityAccessFacade.pagingQueryNotGrantPermissionsByMenuResourceId(currentPage, pageSize, menuResource.getId()).getData();
+		assertFalse(results.getData().isEmpty());
+		assertTrue(results.getPageCount() == 1);
+	}
+
+	@Test
+	public void testPagingQueryNotGrantPermissionsByUserId() {
+		Page<PermissionDTO> results = (Page<PermissionDTO>) securityAccessFacade.pagingQueryNotGrantPermissionsByUserId(currentPage, pageSize, permissionDTO, user.getId()).getData();
+		assertFalse(results.getData().isEmpty());
+		assertTrue(results.getPageCount() == 1);
+	}
+
+	@Test
+	public void testPagingQueryPageElementResources() {
+		Page<PageElementResourceDTO> results = (Page<PageElementResourceDTO>) securityAccessFacade.pagingQueryPageElementResources(currentPage, pageSize, pageElementResourceDTO).getData();
+		assertFalse(results.getData().isEmpty());
+		assertTrue(results.getPageCount() == 1);
+	}
+
+	@Test
+	public void testPagingQueryGrantPageElementResourcesByRoleId() {
+		Page<PageElementResourceDTO> results = (Page<PageElementResourceDTO>) securityAccessFacade.pagingQueryGrantPageElementResourcesByRoleId(currentPage, pageSize, role.getId()).getData();
+		assertFalse(results.getData().isEmpty());
+		assertTrue(results.getPageCount() == 1);
+	}
+
+	@Test
+	public void testPagingQueryNotGrantPageElementResourcesByRoleId() {
+		PageElementResource pageElementResource = new PageElementResource("测试分页查询没有授权的页面元素资源","testPagingQueryNotGrantPageElementResourcesByRoleId");
+		pageElementResource.save();
+		Page<PageElementResourceDTO> results = (Page<PageElementResourceDTO>) securityAccessFacade.pagingQueryNotGrantPageElementResourcesByRoleId(currentPage, pageSize, role.getId()).getData();
+		assertFalse(results.getData().isEmpty());
+		assertTrue(results.getPageCount() == 1);
+	}
+
+	@Test
+	public void testPagingQueryGrantPermissionsByPageElementResourceId() {
+		Page<PermissionDTO> results = (Page<PermissionDTO>) securityAccessFacade.pagingQueryGrantPermissionsByPageElementResourceId(currentPage, pageSize,pageElementResource.getId()).getData();
+		assertFalse(results.getData().isEmpty());
+		assertTrue(results.getPageCount() == 1);
+        PermissionDTO actualPermission = results.getData().get(0);
+        assertNotNull(actualPermission);
+        assertNotNull(actualPermission.getId());
+        assertEquals(permission.getId(),actualPermission.getId());
+        assertEquals(permission.getIdentifier(),actualPermission.getIdentifier());
+        assertEquals(permission.getName(),actualPermission.getName());
+        assertEquals(permission.getDescription(),actualPermission.getDescription());
+	}
+
+	@Test
+	public void testPagingQueryNotGrantPermissionsByPageElementResourceId() {
+		Permission permission = new Permission("testPagingQueryNotGrantPermissionsByPageElementResourceId", "testPagingQueryNotGrantPermissionsByPageElementResourceId");
+		permission.save();
+		Page<PermissionDTO> results = (Page<PermissionDTO>) securityAccessFacade.pagingQueryNotGrantPermissionsByPageElementResourceId(currentPage, pageSize,pageElementResource.getId()).getData();
+		assertFalse(results.getData().isEmpty());
+		assertTrue(results.getPageCount() == 1);
+	}
+
+    @Test
+    public void testPagingQueryRolesOfUser(){
+        InvokeResult invokeResult = securityAccessFacade.pagingQueryRolesOfUser(0, 10, user.getUserAccount());
+        Page<Role> roles = (Page<Role>) invokeResult.getData();
+        assertFalse(roles.getData().isEmpty());
+        assertTrue(roles.getPageCount() == 1);
+    }
+
+}
---
+++ koala-security/koala-security-web/src/main/webapp/pages/auth/role-list.jsp
@@ -0,0 +1,328 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"  pageEncoding="UTF-8"%>
+<%@include file="/commons/taglibs.jsp"%>
+
+<script>
+	$(function() {
+		var tabData 	= $('.tab-pane.active').data();
+		var userId 		= tabData.userId;
+		var columns = [{
+			title : "角色名称",
+			name : "name",
+			width : 250
+		}, {
+			title : "角色描述",
+			name : "description",
+			width : 250
+		}];
+	
+		var buttons = (function() {
+			if (userId) {
+				return [{
+					content : '<button class="btn btn-primary" type="button"><span class="glyphicon glyphicon-th-large"><span>分配角色</button>',
+					action : 'assignRole'
+				}, {
+					content : '<button class="btn btn-danger" type="button"><span class="glyphicon glyphicon-remove"><span>删除</button>',
+					action : 'removeRoleForUser'
+				}];
+			} else {
+				return [{
+					content : '<ks:hasSecurityResource identifier="roleManagerAdd"><button class="btn btn-primary" type="button"><span class="glyphicon glyphicon-plus"><span>添加</button></ks:hasSecurityResource>',
+					action : 'add'
+				}, {
+					content : '<ks:hasSecurityResource identifier="roleManagerUpdate"><button class="btn btn-success" type="button"><span class="glyphicon glyphicon-edit"><span>修改</button></ks:hasSecurityResource>',
+					action : 'modify'
+				}, {
+					content : '<ks:hasSecurityResource identifier="roleManagerTerminate"><button class="btn btn-danger" type="button"><span class="glyphicon glyphicon-remove"><span>删除</button></ks:hasSecurityResource>',
+					action : 'delete'
+				}, {
+					content : '<ks:hasSecurityResource identifier="roleManagerGrantUrlAccessResource"><button class="btn btn-info" type="button"><span class="glyphicon glyphicon-th-large"></span>&nbsp;分配URL访问资源</button></ks:hasSecurityResource>',
+					action : 'urlAssign'
+				}, {
+					content : '<ks:hasSecurityResource identifier="roleManagerGrantMenuResource"><button class="btn btn-info" type="button"><span class="glyphicon glyphicon-th-large"></span>&nbsp;分配菜单资源</button></ks:hasSecurityResource>',
+					action : 'menuAssign'
+				},{
+					content : '<ks:hasSecurityResource identifier="roleManagerGrantPageElementResource"><button class="btn btn-info" type="button"><span class="glyphicon glyphicon-th-large"></span>&nbsp;分配页面元素资源</button></ks:hasSecurityResource>',
+					action : 'pageAssign'
+				},{
+					content : '<ks:hasSecurityResource identifier="roleManagerGrantPermission"><button class="btn btn-info" type="button"><span class="glyphicon glyphicon-th-large"></span>&nbsp;分配权限</button></ks:hasSecurityResource>',
+					action : 'permissionAssign'
+				}];
+			}
+		})();
+		
+		var url = contextPath + '/auth/role/pagingQuery.koala';
+		if (userId) {
+			url = contextPath + '/auth/user/pagingQueryGrantRoleByUserId.koala?userId=' + userId;
+		}
+		
+		$("<div/>").appendTo($("#tabContent>div:last-child")).grid({
+			identity : 'id',
+			columns : columns,
+			buttons : buttons,
+			dataFilter:function(result){
+                return result.data;
+            },
+			url 	: url
+		}).on({
+			'add' 	: function() {
+				roleManager().add($(this));
+			},
+			'modify' : function(event, data) {
+				var indexs = data.data;
+				var $this = $(this);
+				if (indexs.length == 0) {       
+					$this.message({
+						type : 'warning',
+						content : '请选择一条记录进行修改'
+					});
+					return;
+				}
+				if (indexs.length > 1) {
+					$this.message({
+						type : 'warning',
+						content : '只能选择一条记录进行修改'
+					});
+					return;
+				}
+				roleManager().modify(data.item[0], $(this));
+			},
+			'delete' : function(event, data) {
+				var indexs = data.data;
+				var $this = $(this);
+				if (indexs.length == 0) {
+					$this.message({
+						type : 'warning',
+						content : '请选择要删除的记录'
+					});
+					return;
+				}
+				$this.confirm({
+					content : '确定要删除所选记录吗?',
+					callBack : function() {
+						roleManager().deleteRole(data.item, $this);
+					}
+				});
+			},
+			'assignRole' : function() {
+				var grid = $(this);
+				//console.log(grid);
+        		$.get(contextPath + '/pages/auth/select-role.jsp').done(function(data){
+        			var dialog = $(data);
+        			dialog.find('#save').click(function(){
+        				var saveBtn = $(this);
+        				var items = dialog.find('.selectRoleGrid').data('koala.grid').selectedRows();
+        				if(items.length == 0){
+        					dialog.find('.selectRoleGrid').message({
+        						type: 'warning',
+        						content: '请选择要分配的角色'
+        					});
+        					return;
+        				}
+        				
+        				saveBtn.attr('disabled', 'disabled');	
+        				var data = "userId="+userId;
+        				for(var i=0,j=items.length; i<j; i++){
+        					data += "&roleIds="+items[i].id;
+        				}
+        				
+        				$.post(contextPath + '/auth/user/grantRolesToUser.koala', data).done(function(data){
+       						grid.message({
+       							type: 'success',
+       							content: '保存成功'
+       						});
+       						dialog.modal('hide');
+       						grid.grid('refresh');
+        				}).fail(function(data){
+        					saveBtn.attr('disabled', 'disabled');	
+        					grid.message({
+        						type: 'error',
+        						content: '保存失败'
+        					});
+        				});
+        			}).end().modal({
+        				keyboard: false
+        			}).on({
+       					'hidden.bs.modal': function(){
+       						$(this).remove();
+       					},
+       					
+       					'shown.bs.modal': function(){ //弹窗初始化完毕后，初始化url选择表格
+       						var columns = [{
+       							title : "角色名称",
+       							name : "name",
+       							width : 250
+       						}, {
+       							title : "角色描述",
+       							name : "description",
+       							width : 250
+       						}];
+       					
+        					dialog.find('.selectRoleGrid').grid({
+        						 identity: 'id',
+        			             columns: columns,
+        			             querys: [{title: 'roleId', value: 'roleId'}],
+        			             dataFilter:function(result){
+                                    return result.data;
+                                 },
+        			             url: contextPath + '/auth/user/pagingQueryNotGrantRoles.koala?userId='+userId
+        			        });
+       					},
+       					
+       					'complete': function(){
+       						grid.message({
+       							type: 'success',
+       							content: '保存成功'
+       						});
+       						$(this).modal('hide');
+       						grid.grid('refresh');
+       					}
+        			});
+        			 //兼容IE8 IE9
+        	        if(window.ActiveXObject){
+        	           if(parseInt(navigator.userAgent.toLowerCase().match(/msie ([\d.]+)/)[1]) < 10){
+        	        	   dialog.trigger('shown.bs.modal');
+        	           }
+        	        }
+        		});
+			},
+			"urlAssign" : function(event, data){
+				var items 	= data.item;
+				var thiz	= $(this);
+				if(items.length == 0){
+					thiz.message({type : 'warning',content : '请选择一条记录进行操作'});
+					return;
+				} else if(items.length > 1){
+					thiz.message({type : 'warning',content : '只能选择一条记录进行操作'});
+					return;
+				}
+				
+				var role = items[0];
+				/*打开url表格*/
+				openTab('/pages/auth/url-list.jsp', role.name+'的url管理', 'roleManager_' + role.id, role.id, {roleId : role.id});
+			},
+			"menuAssign" : function(event, data) {
+				var items = data.item;
+				var $this = $(this);
+				if (items.length == 0) {
+					$this.message({
+						type : 'warning',
+						content : '请选择一条记录进行操作'
+					});
+					return;
+				}
+				if (items.length > 1) {
+					$this.message({
+						type : 'warning',
+						content : '只能选择一条记录进行操作'
+					});
+					return;
+				}
+				roleManager().assignResource($this, items[0].id);
+			},
+			'pageAssign' : function(event, data) {
+				var items = data.item;
+				var $this = $(this);
+				if (items.length == 0) {
+					$this.message({
+						type : 'warning',
+						content : '请选择一条记录进行操作'
+					});
+					return;
+				}
+				if (items.length > 1) {
+					$this.message({
+						type : 'warning',
+						content : '只能选择一条记录进行操作'
+					});
+					return;
+				}
+				//roleManager().pageAssign($(this), items[0].roleId);
+				var page = items[0];
+				openTab('/pages/auth/page-list.jsp', page.name+'的page管理', 'roleManager_' + page.id, page.id, {pageId : page.id});
+			},
+			'permissionAssign' : function(event, data) {
+				var items = data.item;
+				var $this = $(this);
+				if (items.length == 0) {
+					$this.message({
+						type : 'warning',
+						content : '请选择一条记录进行操作'
+					});
+					return;
+				}
+				if (items.length > 1) {
+					$this.message({
+						type : 'warning',
+						content : '只能选择一条记录进行操作'
+					});
+					return;
+				}
+				//roleManager().pageAssign($(this), items[0].roleId);
+				var permissions = items[0];
+				openTab('/pages/auth/permission-list.jsp', permissions.name+'的权限管理', 'roleManager_' + permissions.id, permissions.id, {permissionsId : permissions.id});
+			}
+			,
+			'removeRoleForUser' : function(event, data) {
+				var indexs = data.data;
+				var grid = $(this);
+				if (indexs.length == 0) {
+					$this.message({
+						type : 'warning',
+						content : '请选择要删除的记录'
+					});
+					return;
+				}
+				grid.confirm({
+					content : '确定要删除所选记录吗?',
+					callBack : function() {
+						var url = contextPath + '/auth/user/terminateAuthorizationByUserInRoles.koala';
+						var params = "userId="+userId;
+						for (var i = 0, j = data.item.length; i < j; i++) {
+							params += ("&roleIds=" + data.item[i].id);
+						}
+						
+						$.post(url, params).done(function(data){
+							if(data.success){
+								grid.message({
+									type: 'success',
+									content: '删除成功'
+								});
+								grid.grid('refresh');
+							}else{
+								grid.message({
+									type: 'error',
+									content: data.errorMessage
+								});
+							}
+						}).fail(function(data){
+							grid.message({
+								type: 'error',
+								content: '删除失败'
+							});
+						});
+					}
+				});
+			},
+			'assignResource' : function(event, data) {
+				var indexs = data.data;
+				var $this = $(this);
+				if (indexs.length == 0) {
+					$this.message({
+						type : 'warning',
+						content : '请选择一条记录进行操作'
+					});
+					return;
+				}
+				if (indexs.length > 1) {
+					$this.message({
+						type : 'warning',
+						content : '只能选择一条记录进行操作'
+					});
+					return;
+				}
+				console.log(data.data[0]);
+				roleManager().assignResource($(this), data.data[0]);
+			}
+		});
+	});
+</script>
