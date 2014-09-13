a8cbb1fed9c36ede079830515848d2510b40f151
liujun <liujun2008116@gmail.com>
2014-09-13 20:01:28 +0800

    update: 删除koala项目源码，添加shanlan-user项目

--- koala-commons/koala-commons-dddlib-mybatis/src/test/java/me/lingen/EmployeeApplication.java
+++
@@ -1,9 +0,0 @@
-package me.lingen;
-
-import org.dayatang.querychannel.Page;
-
-public interface EmployeeApplication {
-
-	public Page<Employee> findEmployeeByName(String name, int currentPage,
-			int pageSize);
-}
