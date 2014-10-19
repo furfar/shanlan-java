a73124fe730b9fd473197893f7fe86f88408aaaf
AlbertLiu <liujun2008116@gmail.com>
2014-10-19 18:10:59 +0800

    dele: 删除无用的文件

--- shanlan-common/shanlan-common-core/src/main/java/com/shanlan/basic/core/domain/Derived1.java
+++
@@ -1,8 +0,0 @@
-package com.shanlan.trade.core.domain;
-
-@Entity
-@PrimaryKeyJoinColumn(name="id", referencedColumnName="id")   
-public class Derived1 extends Base {   
-    @Basic  
-    private String derived1Name;   
-}  
--- shanlan-common/shanlan-common-core/src/main/java/com/shanlan/basic/core/domain/Derived2.java
+++
@@ -1,8 +0,0 @@
-package com.shanlan.trade.core.domain;
-
-@Entity
-@PrimaryKeyJoinColumn(name="id", referencedColumnName="id")   
-public class Derived2 extends Base {   
-    @Basic  
-    private String derived2Name;   
-}  
